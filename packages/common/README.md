4a1fbf8e9d170cbd14cf071479983793b5131b74
anijs6 <anijs666@hotmail.com>
2021-11-12 22:42:23 +0800

    feat(*): init commont config

---
+++ packages/common/README.md
@@ -0,0 +1,6 @@
+# @anijs/eslint-config-common
+
+所有`eslint`预设的基础配置，包含以下的配置：
+
+1. `eslint-plugin-jsdoc`：`jsdoc`注释规范
+2. `eslint-plugin-html`：添加对`html`的支持
---
+++ packages/common/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: ['plugin:jsdoc/recommended'],
+  plugins: ['jsdoc', 'html']
+}
